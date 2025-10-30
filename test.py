efc1312d4d07bbed914140ffc31079d90de28910
charan97 <pradeeppulicherla97@gmail.com>
2025-10-30 14:09:31 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,22 @@
+def fib(n: int) -> int:
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+def print_fib_series(terms: int) -> None:
+    if terms <= 0:
+        print("Please enter a positive number of terms.")
+        return
+    series = [str(fib(i)) for i in range(terms)]
+    print("Fibonacci series ({} terms):".format(terms))
+    print(" ".join(series))
+
+if __name__ == "__main__":
+    try:
+        n = int(input("Enter number of terms: ").strip())
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+    else:
+        print_fib_series(n)
